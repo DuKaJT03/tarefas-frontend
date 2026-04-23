3f10de9bd8c92686d0b2cf0774ea893087b3937f
DuKaJT03 <jhonticona2001@gmail.com>
2026-04-23 20:28:19 -0300

    frontend tarefas

---
+++ script.js
@@ -0,0 +1,116 @@
+const API = "https://tarefas-api-8ein.onrender.com/tarefas";
+
+//Listar tarefas
+function carregarTarefas() {
+    fetch(API)
+        .then(response => response.json())
+        .then(dados => {
+            const lista = document.getElementById("listaTarefas");
+            lista.innerHTML = "";
+
+            dados.forEach(tarefa => {
+                const item = document.createElement("li");
+
+                item.innerHTML = `
+                    <strong>${tarefa.titulo}</strong><br>
+                    ${tarefa.descricao}<br>
+                    ${tarefa.data}<br>
+                    Status: ${tarefa.status}<br>
+                    Concluida: ${tarefa.concluida ? "Sim" : "Não"}<br>
+
+                    <button onclick="editar(
+                        ${tarefa.id}, 
+                        \`${tarefa.titulo}\`, 
+                        \`${tarefa.descricao}\`,
+                        \`${tarefa.data}\`,
+                        \`${tarefa.status}\`,
+                        ${tarefa.concluida}
+                    )">Editar</button>
+                    <button onclick="deletar(${tarefa.id})">X</button>
+                `;
+
+                lista.appendChild(item);
+            });
+        });
+}
+
+// CRIAR TAREFA
+function criarTarefa(){
+    const titulo = document.getElementById("titulo").value;
+    const descricao = document.getElementById("descricao").value;
+    const status = document.getElementById("status").value;
+    const data = document.getElementById("data").value;
+    const concluida = document.getElementById("concluida").checked;
+    
+    if(!titulo || !descricao){
+        alert("Preencher todos os campos!");
+        return;
+    }
+
+    fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: descricao,
+            data: data,
+            status: status,
+            concluida: concluida
+        })
+    })
+    .then(() => {
+        alert("Tarefa criada com sucesso!");
+        carregarTarefas()});
+
+    document.getElementById("titulo").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("data").value = "";
+    document.getElementById("concluida").checked = false;
+}
+
+//EDITAR
+function editar(id, titulo, descricao, data, status, concluida){
+    const novoTitulo = prompt("Novo título:", titulo);
+    const novaDescricao = prompt("Nova descrição:", descricao);
+
+    if(!novoTitulo || !novaDescricao){
+        alert ("Título e descrição são obrigatórios!");
+        return;
+    }
+
+    fetch(`${API}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: novoTitulo,
+            descricao: novaDescricao,
+            data: data,
+            status: status,
+            concluida: concluida
+        })
+    })
+    .then(response => {
+        if(!response.ok){
+            alert("Erro ao atualizar tarefa!");
+            return;
+        }
+        carregarTarefas();
+    });
+}
+
+//DELETAR
+function deletar(id){
+    if(confirm("Tem certeza que deseja deletar?")){
+        fetch(`${API}/${id}`, {
+            method: "DELETE"
+        })
+        .then(() => carregarTarefas());
+    }
+}
+
+// CARREGAR AO ABRIR
+carregarTarefas();
